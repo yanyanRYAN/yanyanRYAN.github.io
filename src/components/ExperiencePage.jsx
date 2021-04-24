55509f0a279c1b70e8f722c7621b9d98179d890e
yanyanRYAN <rpecarma@gmail.com>
2021-04-24 17:57:36 -0500

    refactored resume.js structure and refactored experience page

--- src/components/ExperiencePage.jsx
+++ src/components/ExperiencePage.jsx
@@ -2,75 +2,23 @@ import React from 'react';
 
 export default function ExperiencePage(props) {
 
-    const experience = props.resumeData;
+    const resumeData = props.resumeData
     
 
     console.log("Exp resumeData");
-    console.log(props.resumeData);
+    console.log(resumeData);
 
-    return(<div>
-    <h1>Experience</h1>
-    <h4>Experience</h4>
-                    <hr />
-                    <div>
-                        <h5>Application Developer 1</h5>
-                        <p>Comcast - Oakbrook Terrace, IL</p>
-                        <ul>
-                            <li>
-                                Deliver new implementations between iterations using .NetCore C#, Entity Framework, HTML,
-                                CSS, JavaScript, Typescript, and Angular, with Azure DevOps Server for documenting progress
-                                and version control
-                            </li>
-                            <li>
-                                Assist with managing company SharePoint sites and SharePoint forms
-                            </li>
-                            <li>
-                                Develop new electronic forms using Microsoft PowerApps and PowerAutomate
-                            </li>
-                            <li>
-                                Update exiting forms from previous SharePoint releases to SharePoint 2019
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h5>Graduate Assistant</h5>
-                        <p>Lewis University Institutional Research & Planning - Romeoville, IL</p>
-                        <ul>
-                            <li>
-                                Assist Associate Vice President for Institutional Research and Planning with internal projects
-                            </li>
-                            <li>
-                                Compile SQL output data from data warehouse into Excel and creating reports
-                            </li>
-                            <li>
-                                Create program review templates for departments
-                            </li>
-                            <li>
-                                Compile Enrollment Headcounts and Credit Hour Production semester reports for IR Portal
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h5>IT Desktop Technician Intern</h5>
-                        <p>Lewis University Office of Technology - Romeoville, IL</p>
-                        <ul>
-                            <li>
-                                Supervised a team of desktop technician student workers
-                            </li>
-                            <li>
-                                Consulted with supervisor for problem solving and troubleshooting issues
-                            </li>
-                            <li>
-                                Performed asset management of equipment through Solarwinds Web Helpdesk and Microsoft Excel
-                            </li>
-                            <li>
-                                Documented and resolved issues and requests submitted through Solarwinds Web Helpdesk
-                            </li>
-                            <li>
-                                Imaged, configured, and deployed desktop PCs and laptops with GhostCast Server and installed 
-                                required software
-                            </li>
-                        </ul>
-                    </div>
-    </div>)
+    return(
+    <div>
+        <h4>Experience</h4>
+
+        {resumeData.map((data, i) => 
+        <div key={i}>{console.log(data)}
+            <h5>{data.position} - {data.name} </h5>
+            <h6>{data.location}</h6>
+            <p>{data.summary}</p>
+        </div>)}
+
+    </div>
+    )
 }
